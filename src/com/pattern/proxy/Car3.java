ab13313dbee9685235b29645cd252e2d52499628
WY08271 <wy08271@outlook.com>
2016-02-18 10:15:19 +0800

    using the aggregationTo implement proxy

---
+++ src/com/pattern/proxy/Car3.java
@@ -0,0 +1,22 @@
+package com.pattern.proxy;
+
+public class Car3 implements Moveable{
+    private Car car;
+
+    public Car3(Car car){
+        super();
+        this.car = car;
+    }
+
+    @Override
+    public void move() {
+        long starttime = System.currentTimeMillis();
+        System.out.println("开始行驶..");
+
+        car.move();
+
+        long endtime = System.currentTimeMillis();
+        System.out.println("结束行驶..行驶时间:"
+                + (endtime - starttime) + "毫秒");
+    }
+}
--- src/com/pattern/proxy/Test.java
+++ src/com/pattern/proxy/Test.java
@@ -2,10 +2,14 @@ package com.pattern.proxy;
 
 public class Test {
     public static void main(String[] args){
-        Car car = new Car();
-        car.move();
+//        Car car = new Car();
+//        car.move();
 
-        Car2 car2 = new Car2();
-        car2.move();
+//        Car2 car2 = new Car2();
+//        car2.move();
+
+        Car car = new Car();
+        Car3 car3 = new Car3(car);
+        car3.move();
     }
 }
